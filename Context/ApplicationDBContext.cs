fd729e123613d6e19ace1c6aaef5b5d636ef6c24
Arnoud-Bury <arnoud.bury@student.hogent.be>
2025-01-06 16:50:30 +0100

    Added EntityFrameworkCore + connection to database

---
+++ Context/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BP_Document_Generation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BP_Document_Generation.Context {
+    public class ApplicationDBContext : DbContext {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
+
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using BP_Document_Generation.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
